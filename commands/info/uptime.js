6c13a9f2b38557c6a66398b43d13d2999e39de08
cgolden15 <61284764+cgolden15@users.noreply.github.com>
2020-12-09 12:46:10 -0500

    Updated console logs

--- commands/info/uptime.js
+++ commands/info/uptime.js
@@ -1,4 +1,5 @@
 const { Client, Collection } = require("discord.js");
+const chalk = require('chalk');
 
 module.exports = {
     name: 'uptime',
@@ -6,6 +7,14 @@ module.exports = {
     category: "info",
     aliases: ["uptime","ut","Uptime","UT"],
     run: async (client, message, args) => {
-        const msg = await message.channel.send(`${client.user.username} has been online for ${client.uptime / 60000} minutes.`)
+        const msg = await message.channel.send(`${client.user.username} has been online for ${Math.round(client.uptime / 60000)} minutes.`)
+        
+        if(client.uptime >= 60000 && client.uptime < 3600000){
+            console.log(chalk.blue(`${new Date().toLocaleString()} - Command run: Uptime [${Math.round(client.uptime / 60000)} minutes]`));
+        } if(client.uptime < 60000){
+            console.log(chalk.blue(`${new Date().toLocaleString()} - Command run: Uptime [${Math.round(client.uptime / 1000)} seconds]`))
+        } if(client.uptime > 3600000){
+            console.log(chalk.blue(`${new Date().toLocaleString()} - Command run Uptime [${Math.round(client.uptime / 3600000)} hours]`))
+        }
     }
 }
