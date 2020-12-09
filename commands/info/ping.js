97fd1fdef09d9e337ef439e0f6a4df4d28688bf8
cgolden15 <61284764+cgolden15@users.noreply.github.com>
2020-12-09 12:58:00 -0500

    Updated pin
    Chalk Support + Latency Warnings

--- commands/info/ping.js
+++ commands/info/ping.js
@@ -1,13 +1,22 @@
+const chalk = require('chalk');
+
 module.exports = {
     name: "ping",
     description: "Returns latency and API ping",
     category: "info",
+    aliases: ["ping", "pong", "latency"],
     run: async (client, message, args) => {
         const msg = await message.channel.send(`🏓 Pinging....`);
 
         msg.edit(`🏓 Pong!
-        Latency is ${Math.floor(msg.createdTimestamp - message.createdTimestamp)}ms
-        API Latency is ${Math.round(client.ping)}ms`);
-        console.log("\x1b[34m%s\x1b[0m" ,"Command run: Ping")
+        Discord latency is ${Math.floor(msg.createdTimestamp - message.createdTimestamp)}ms
+        API latency is ${Math.round(client.ping)}ms`);
+
+        if(client.ping <= 250){
+        console.log(chalk.blue(`${new Date().toLocaleString()} - Command run: Ping [${Math.round(client.ping)}ms]`));
+         
+        } else {
+            console.log(chalk.bgRedBright.black(`${new Date().toLocaleString()} - WARNING: Client Ping over 250ms! [${client.ping}ms]`))
+        }
+
     }
-}
