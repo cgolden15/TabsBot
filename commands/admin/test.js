c9bb18af84c9340a492e68aef79048f76ae11ab9
cgolden15 <61284764+cgolden15@users.noreply.github.com>
2020-12-10 10:03:27 -0500

    Update test.js

--- commands/admin/test.js
+++ commands/admin/test.js
@@ -4,13 +4,13 @@ const { Client, Collection } = require("discord.js");
 let Developers = ["317283391982534666"]
 
 module.exports = {
-    name: "Test",
-    description: "Testing",
+    name: "Kill",
+    description: "Stops the bot. Use in caase of emergency.",
     category: "admin",
+    aliases: ["kill", "godie"],
     run: async (client, message, args) => {
-        Developers.includes(message.author.id);{
+        if(Developers.includes(message.author.id));{
             const msg = await message.channel.send('You own the bot')
         }
-    
     }
-};
+}
